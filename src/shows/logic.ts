f5aebdf3fd8e0668bba8c4b12bed7fa9e1986123
Samuel Macleod <samuel@macleod.space>
2020-10-13 15:15:30 +0100

    Dates are _weird_

--- src/shows/logic.ts
+++ src/shows/logic.ts
@@ -295,7 +295,7 @@ export const setMeta = async (
     console.log("Scheduling episode", {
       url: meta.audio,
       time: `${moment(episode.scheduling.week)
-        .add(show.meta.day + 1, "day")
+        .add(show.meta.day + 2, "day")
         .format("YYYY-MM-DD")} ${show.meta.time}:00`,
       length: meta.length,
       name: episode?.title ?? "Episode x",
